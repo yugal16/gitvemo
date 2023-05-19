1b1ea8ae10fffd50ac1c82c866c1e1a0fcd61c05
Yugal Bajaj <yyugalbajajj@gmail.com>
2023-05-20 02:35:22 +0530

    commited

--- gitvemo2.py
+++ gitvemo2.py
@@ -1,7 +1,8 @@
-<<<<<<< HEAD
 print('hello_raghav')
-=======
-print('hallo_baba')
-print('maddy')
-print('practice')
->>>>>>> b022beeaefeaac6989042a48b792e46f15594fb1
+
+
+
+
+
+
+print('line 8 in local')
