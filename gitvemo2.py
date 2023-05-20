58dfb1f204ebd71aa55d830299e346791c7ad308
Yugal Bajaj <yyugalbajajj@gmail.com>
2023-05-21 00:23:40 +0530

    commit

---
+++ gitvemo2.py
@@ -0,0 +1,23 @@
+print('hello_raghav')
+
+
+
+
+
+
+print('line 8 in local')
+=======
+
+print('maddy')
+print('practice')
+print('bhai lfada hogya')
+>>>>>>> 4a75df1d273d51a92e784b49cb5e622a7db69466
+print('btao tereko gadhe')
+print('ki krlega')
+print('bolo tararaa')
+print('pagol')
+<<<<<<< HEAD
+print('line 19 in local')
+=======
+print('line 19 in github')
+>>>>>>> dec91c36f8b4834c8180e94856d99f8af9c3f587
