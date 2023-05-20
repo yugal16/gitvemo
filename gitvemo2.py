0694d6b9ed4aff538977e710f9d2156a01196d74
yugal16 <70807914+yugal16@users.noreply.github.com>
2023-05-21 00:10:38 +0530

    Update gitvemo2.py

--- gitvemo2.py
+++ gitvemo2.py
@@ -10,7 +10,7 @@ print('line 8 in local')
 print('hallo_baba')
 print('maddy')
 print('practice')
-print(line 8 in local')
+print('bhai lfada hogya')
 >>>>>>> 4a75df1d273d51a92e784b49cb5e622a7db69466
 print('btao tereko gadhe')
 print('ki krlega')
